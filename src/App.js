27e8040d83aea096f068d659eb226cc1221a6c57
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-09-29 05:28:32 +0200

    router in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import './App.scss';
 import Home from './components/Home';
+import Settings from './components/Settings';
 import { ThemeContext } from './themeContext';
+import { BrowserRouter as Router, NavLink, Switch, Route } from 'react-router-dom';
 
 function App() {
 	const [theme, setTheme] = useState('dark');
 	return (
 		<div className="App">
 			<ThemeContext.Provider value={{ theme, setTheme }}>
-				<Home />
+				<Router>
+					<nav>
+						<ul>
+							<li><NavLink to="/">Home</NavLink></li>
+							<li><NavLink to="/settings">Settings</NavLink></li>
+						</ul>
+					</nav>
+					<Switch>
+						<Route path="/" exact><Home /></Route>
+						<Route path="/settings"><Settings/></Route>
+					</Switch>
+				</Router>
 			</ThemeContext.Provider>
 		</div>
 	);
