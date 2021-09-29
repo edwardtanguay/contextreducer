b0d72c1dfde960a3b562837dbf6152bd6f2ebb87
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-09-29 05:58:34 +0200

    Theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Home from './components/Home';
 import Settings from './components/Settings';
 import { ThemeContext } from './themeContext';
 import { BrowserRouter as Router, NavLink, Switch, Route } from 'react-router-dom';
 
+
 function App() {
 	const [theme, setTheme] = useState('dark');
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.body.style = 'background: #333;color: #fff;';
+		} else {
+			document.body.style = 'background: #fff;color: black;';
+		}
+	}, [theme]);
+
 	return (
 		<div className="App">
 			<ThemeContext.Provider value={{ theme, setTheme }}>
@@ -19,7 +29,7 @@ function App() {
 					</nav>
 					<Switch>
 						<Route path="/" exact><Home /></Route>
-						<Route path="/settings"><Settings/></Route>
+						<Route path="/settings"><Settings /></Route>
 					</Switch>
 				</Router>
 			</ThemeContext.Provider>
